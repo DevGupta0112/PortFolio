32f68e979f9f6a2358043f49b2544aa65d6089a8
Dev Kumar Gupta <138769328+DevGupta0112@users.noreply.github.com>
2024-07-07 10:00:42 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("https://devg-portfolio-1.vercel.app/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
